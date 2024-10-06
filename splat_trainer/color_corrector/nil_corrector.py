eae53301365b099772bbb74321554b044c61fa53
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2024-10-06 14:08:51 +1300

    Some refactoring

--- splat_trainer/color_corrector/nil_corrector.py
+++ splat_trainer/color_corrector/nil_corrector.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Dict, Tuple
 
 import torch
 from taichi_splatting import Rendering
@@ -10,23 +11,21 @@ from .corrector import CorrectorConfig, Corrector
 @dataclass
 class NilCorrectorConfig(CorrectorConfig):
 
-  def make_corrector(self, num_images:int) -> Corrector:
-    return NilCorrector()
+  def make_corrector(self, num_images:int, device:torch.device) -> Corrector:
+    return NilCorrector(device)
 
 
 class NilCorrector(Corrector):
-  def __init__(self):
-    self.value = False
+  def __init__(self, device:torch.device):
+    self.device = device
 
-  def correct(self, name:str, rendering:Rendering, image:torch.Tensor, image_idx:int) -> Rendering:
-    return rendering
+  def correct(self, rendering:Rendering, image_idx:int) -> Rendering:
+    return rendering.image
 
-  def step(self):
+  def step(self, t:float):
     pass
 
-  def loss(self) -> float:
-    return 0.
+  def loss(self) -> Tuple[torch.Tensor, Dict[str, float]]:
+    return torch.tensor(0.0, device=self.device), {}
 
-  def __bool__(self) -> bool:
-    return self.value
   
