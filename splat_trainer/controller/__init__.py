1e225edd9ebbf026d9fb099855216c425af17384
Oliver Batchelor <saulzar@gmail.com>
2024-12-27 20:14:08 +1300

    .

--- splat_trainer/controller/__init__.py
+++ splat_trainer/controller/__init__.py
@@ -1,6 +1,9 @@
-from .controller import ControllerConfig, Controller
+from .controller import ControllerConfig, Controller, DisabledConfig, DisabledController
 
 from .target_controller import TargetConfig, TargetController
 from .threshold_controller import ThresholdConfig, ThresholdController
 
-__all__ = ['ControllerConfig', 'Controller', 'TargetConfig', 'TargetController', 'ThresholdConfig', 'ThresholdController']
+__all__ = ['ControllerConfig', 'Controller', 
+           'TargetConfig', 'TargetController', 
+           'ThresholdConfig', 'ThresholdController',
+           'DisabledConfig', 'DisabledController']
